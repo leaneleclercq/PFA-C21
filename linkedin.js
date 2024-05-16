b19116554cd2d347155436dce3b447fc6f2a82f2
ThomasSouly1409 <thomas.souly@gmail.com>
2024-05-16 16:46:38 +0200

    Update linkedin.js
    linkedin V2

--- linkedin.js
+++ linkedin.js
@@ -22,8 +22,21 @@ const url = "https://www.linkedin.com/?trk=seo-authwall-base_nav-header-logo";
   // login
   await page.type('[name=session_key]', "cindy.alves0304@gmail.com", { delay: 100 });
   await page.type('[name=session_password]', "dydylele00", { delay: 100 });
-
   await page.click("button[type=submit]");
+
+  // search bar
+  await page.type('[aria-label=Recherche]', "leane leclercq", { delay: 100 }); // "data[nom], data[prénom], data[agence]"
+  await page.keyboard.press('Enter');
+
+  // research personne
+  await page.click("button[class=artdeco-pill artdeco-pill--slate artdeco-pill--choice artdeco-pill--2 search-reusables__filter-pill-button search-reusables__filter-pill-button]");
+  await page.click("reusable-search__result-container > reusable-search__entity-result-list list-style-none");
+
+  // contact
+  await page.click("artdeco-button artdeco-button--2 artdeco-button--secondary ember-view");
+  await page.click("artdeco-button artdeco-button--muted artdeco-button--2 artdeco-button--secondary ember-view mr1");
+  await page.type('[class=ember-text-area ember-view connect-button-send-invite__custom-message mb3]', "vtf leane", { delay: 100 });
+  await page.click("class=artdeco-button artdeco-button--2 artdeco-button--primary artdeco-button--disabled ember-view ml1");
 
   // auto connect
   //await page.waitForSelector(".cmbtv > button", { visible: true });
